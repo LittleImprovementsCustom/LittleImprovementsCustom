2c526c47af251d31882a21b2db8571e3ce272d26
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-07-26 20:47:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Http = new XMLHttpRequest();
+const url='https://jsonplaceholder.typicode.com/posts';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
