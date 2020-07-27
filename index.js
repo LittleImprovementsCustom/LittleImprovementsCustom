4a62dff393b9c25c78014ceab2b6485c49e6c7d2
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-07-27 09:51:43 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const Http = new XMLHttpRequest();
-const url='https://jsonplaceholder.typicode.com/posts';
-Http.open("GET", url);
-Http.send();
-
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
-}
