612f17bbc4d148e0ff11223f8c65521e30276426
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-07-28 08:31:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ const server = http.createServer(function (req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<h1>Hello World</h1>');
-  console.log(`Server running at port `+port);
-  if (req.method === "POST") {
-    console.log("received post request")
-  }
-}).listen(3000);
+});
 
-/*server.listen(port,() => {
+server.listen(port,() => {
   console.log(`Server running at port `+port);
-});*/
+});
