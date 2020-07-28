e977f5531eb5c5d12855955755ecc09d219e2466
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-07-28 08:55:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const http = require('http');
 const port = process.env.PORT || 3000
 
 const server = http.createServer(function (req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  if (req.method==="POST") {
+    console.log("received post request")
+  }
+  else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+  }
 });
 
 server.listen(port,() => {
