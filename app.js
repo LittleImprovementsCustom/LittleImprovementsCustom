98ca320c4271c4c5801313121979d16dd5431f7f
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-07-28 15:07:08 +0100

    added querystring and tried to get data

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+var qs = require('querystring')
+
+
 const http = require('http');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.method==="POST") {
     console.log("received post request")
+    var body = '';
+
+    req.on('data', function (data) {
+        body += data;
+
+        // Too much POST data, kill the connection!
+        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+        if (body.length > 1e6)
+            req.connection.destroy();
+    });
+
+    req.on('end', function () {
+        var post = qs.parse(body);
+        // use post['blah'], etc.
+    });
+    
     res.end()
   }
   else {
