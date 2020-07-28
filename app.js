1814d0eba3c8f971495209d127f974a8226bdccd
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-07-28 08:22:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const http = require('http');
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(function (req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<h1>Hello World</h1>');
-});
-
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
   if (req.method === "POST") {
     console.log("received post request")
   }
+}).listen(3000);
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
