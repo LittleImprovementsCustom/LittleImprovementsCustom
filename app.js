854101b81bbb1d63447d16394dee44a4e2fe4ce9
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-09 19:59:48 +0100

    moved to express

--- app.js
+++ app.js
@@ -1,43 +1,15 @@
 const qs = require('querystring')
+const express = require("express")
+var app = express()
 //const fs = require('fs')
 
-const http = require('http');
-const port = process.env.PORT || 3000
+app.use(express.json());       // to support JSON-encoded bodies
 
-const server = http.createServer((req, res) => {
-  if (req.method==="POST") {
-    console.log("received post request")
-    var body = '';
 
-    // hi
+app.post('/', function (req, res) {
+  var factory = req.body.factory;
+  console.log("factory="+factory)
+  res.send('POST request to homepage');
+})
 
-    req.on('data', function (data) {
-        body += data;
-        console.log("data="+data)
-        //if (data=="NEW") { fs.mkdir("test") }
-
-        // Too much POST data, kill the connection!
-        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-        if (body.length > 1e6)
-            req.connection.destroy();
-    });
-    
-    console.log("thedata="+body)
-    
-    req.on('end', function () {
-        var post = qs.parse(body);
-        console.log(post);
-    });
-
-    res.end()
-  }
-  else {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-  }
-});
-
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+app.listen(3000)
