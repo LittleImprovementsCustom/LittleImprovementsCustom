f5c8d5498efdc7a57bf018f0472c5a63f2239c01
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-11 22:16:54 +0100

    Update compile.js

--- server/compile.js
+++ server/compile.js
@@ -1,5 +1,6 @@
 const shortid = require("shortid");
-shortid.length = 4
+//shortid.length = 4
+console.log("length="+shortid.length)
 
 module.exports.compilePack  = function(requestBody) {
     id = shortid.generate()
