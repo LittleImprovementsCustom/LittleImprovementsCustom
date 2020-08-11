219f06fc4e908859ad1987e3381e83429c376286
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-11 22:44:33 +0100

    create folder with id name when compiling

--- server/compile.js
+++ server/compile.js
@@ -1,8 +1,9 @@
+// require modules
 const Nanoid = require ("nanoid");
-
 const Dropbox = require("dropbox").Dropbox;
 require("isomorphic-fetch");
 require("dotenv").config();
+
 const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN });
 dbx.filesListFolder({path: ''})
     .then(function(response) {
@@ -15,4 +16,12 @@ dbx.filesListFolder({path: ''})
 module.exports.compilePack  = function(requestBody) {
     id = Nanoid.nanoid(4)
     console.log("id="+id)
+    
+    dbx.filesCreateFolderV2({path: `/packs/${id}`})
+    .then(function(response) {
+    console.log(response);
+    })
+    .catch(function(error) {
+    console.error(error);
+    });
 }
