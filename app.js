0a0326eaf15dd3996b4afb211b58aa2eab45eecf
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-11 22:43:59 +0100

    remove dropbox from app.js

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
 // require modules
 const express = require("express");
-const Dropbox = require("dropbox").Dropbox;
 const compileScript = require("./server/compile");
-require("isomorphic-fetch");
-require("dotenv").config();
 
 // setup express
 const app = express();
 app.use(express.json()); // to support JSON-encoded bodies
-
-// setup dropbpx
-const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN });
-
-dbx.filesListFolder({path: ''})
-    .then(function(response) {
-      console.log(response.entries);
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
-/*
-dbx.filesCreateFolderV2({path: "/potatocheese"})
-.then(function(response) {
-  console.log(response);
-})
-.catch(function(error) {
-  console.error(error);
-});*/
 
 // webpage the user sees, on a get request
 app.get("/", function (req, res) {
