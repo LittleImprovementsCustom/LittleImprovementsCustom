8d2d717d1815d301cfacea0983f3c514924736f2
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-12 20:18:29 +0100

    Update honeyJar.js

--- server/addModules/honeyJar.js
+++ server/addModules/honeyJar.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN });
 
 module.exports.addModule = function(packPath) {
-    const image = fs.readFileSync("/storage/modules/honeyJar/textures/item/honey_bottle.png")
+    const image = fs.readFileSync("storage/modules/honeyJar/textures/item/honey_bottle.png")
     dbx
     .filesUpload({
       path: `/${image.name}`,
