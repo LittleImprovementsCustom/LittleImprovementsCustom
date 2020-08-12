45599ee652959aa78996ab87470edd148d8926a4
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-12 19:12:43 +0100

    Update honeyJar.js

--- server/addModules/honeyJar.js
+++ server/addModules/honeyJar.js
@@ -10,7 +10,7 @@ module.exports.addModule = function(packPath) {
         parameters: {
             path: (packPath+"/assets/minecraft/textures/item/honey_bottle.png")
         },
-        readStream: fs.createReadStream("/assets/minecraft/textures/item/honey_bottle.png")
+        readStream: fs.createReadStream("../storage/modules/honeyJar/textures/item/honey_bottle")
     }, (err, result, response) => {
         //upload completed
     });
