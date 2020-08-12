05b95f521f9996883834b40eb7c3bb262516002b
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-12 19:00:20 +0100

    stop listing folders when honeyJar module is added

--- server/addModules/honeyJar.js
+++ server/addModules/honeyJar.js
@@ -4,11 +4,5 @@ require("dotenv").config();
 const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN });
 
 module.exports.addModule = function() {
-    dbx.filesListFolder({path: ''})
-    .then(function(response) {
-    console.log(response.entries);
-    })
-    .catch(function(error) {
-    console.error(error);
-    });
+
 }
