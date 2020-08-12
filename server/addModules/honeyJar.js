d8d10c518ec9f74ecdc355d19a83ad37fda00c08
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-12 18:59:15 +0100

    stop listing folders in addModule script

--- server/addModules/honeyJar.js
+++ server/addModules/honeyJar.js
@@ -2,13 +2,6 @@ const Dropbox = require("dropbox").Dropbox;
 require("isomorphic-fetch");
 require("dotenv").config();
 const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN });
-dbx.filesListFolder({path: ''})
-.then(function(response) {
-	console.log(response.entries);
-})
-.catch(function(error) {
-	console.error(error);
-});
 
 module.exports.addModule = function() {
     dbx.filesListFolder({path: ''})
