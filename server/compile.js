522048d3c918e0681852a85f6bc3351054a3f86c
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-12 17:17:47 +0100

    Update compile.js

--- server/compile.js
+++ server/compile.js
@@ -1,27 +1,41 @@
-// require modules
-const Nanoid = require ("nanoid");
+// require npm modules
+const Nanoid = require("nanoid");
 const Dropbox = require("dropbox").Dropbox;
 require("isomorphic-fetch");
 require("dotenv").config();
 
+// require addModule files
+const honeyJar = require("./addModules/honeyJar")
+
+// setup Dropbox
 const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN });
 dbx.filesListFolder({path: ''})
-    .then(function(response) {
-      console.log(response.entries);
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
+.then(function(response) {
+	console.log(response.entries);
+})
+.catch(function(error) {
+	console.error(error);
+});
 
-module.exports.compilePack  = function(requestBody) {
-    id = Nanoid.nanoid(4)
-    console.log("id="+id)
-    
-    dbx.filesCreateFolderV2({path: `/packs/${id}`})
+// function to create folder
+function createFolder (folderPath) {
+    dbx.filesCreateFolderV2({path: folderPath})
     .then(function(response) {
     console.log(response);
     })
     .catch(function(error) {
     console.error(error);
     });
+}
+
+// compilePack function that gets exported to app.js
+module.exports.compilePack  = function(requestBody) {
+
+	// generate id
+    id = Nanoid.nanoid(4)
+    console.log("id="+id)
+	
+	// create pack folder
+    createFolder(`/packs/${id}`);
+	
 }
