2edafd609535346e25bb1b66b263adc4158ff8cc
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-12 19:06:39 +0100

    Update honeyJar.js

--- server/addModules/honeyJar.js
+++ server/addModules/honeyJar.js
@@ -3,6 +3,14 @@ require("isomorphic-fetch");
 require("dotenv").config();
 const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN });
 
-module.exports.addModule = function() {
-
+module.exports.addModule = function(packPath) {
+    dropbox({
+        resource: "../storage/modules/honeyJar/textures/item/honey_bottle",
+        parameters: {
+            path: (packPath+"/assets/minecraft/textures/item/honey_bottle.png")
+        },
+        readStream: fs.createReadStream("/assets/minecraft/textures/item/honey_bottle.png")
+    }, (err, result, response) => {
+        //upload completed
+    });
 }
