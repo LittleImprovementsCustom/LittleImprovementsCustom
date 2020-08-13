6c3081ba0a2c40fb247bcc0f797bafcad8f01f9c
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-13 21:35:59 +0100

    removed skeleton fodler creation

--- server/compile.js
+++ server/compile.js
@@ -13,27 +13,8 @@ const honeyJar = require("./addModules/honeyJar")
 const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN });
 module.exports.dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN });;
 
-// function to create folder
-function createFolder (folderPath) {
-    dbx.filesCreateFolderV2({path: folderPath})
-    .then(function(response) {
-    console.log(response);
-    })
-    .catch(function(error) {
-    console.error(error);
-	});
-	// sleep to avoid namespace lock contentions
-	sleep(100);
-}
-
 // delcare values for lists of modules
 const mainModules = modulesList.mainModules
-
-// delcare constant for folders that need to be created in a pack
-const skeletonFoldersToCreate = [
-	"textures/item",
-	"textures/block"
-]
 
 // compilePack function that gets exported to app.js
 module.exports.compilePack  = function(requestBody) {
@@ -45,12 +26,6 @@ module.exports.compilePack  = function(requestBody) {
 	// create value for invidual pack path
 	const packPath = `/packs/${id}`
 	console.log("pack path = "+packPath)
-
-	// create pack folder
-	createFolder(packPath);
-	
-	// create skeleton folder structure
-	//for (i in skeletonFoldersToCreate) {createFolder(packPath+"/assets/minecraft/"+ skeletonFoldersToCreate[i])}
 
 	// go through every available module, and if it is included in the request body, run the function to add it
 	if ( requestBody.modules.includes ("honeyJar") ) {
