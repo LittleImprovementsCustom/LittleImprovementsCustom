5e7b710b8fd56fdecfc02c1b8a770a16ce2b6beb
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-08-25 11:32:35 +0100

    make everything async await

--- app.js
+++ app.js
@@ -27,13 +27,44 @@ function uploadFile (storageFilePath,packFilePath,packRoot) {
 }
 
 async function uploadMultipleFiles (storageFilePaths,packFilePaths,packRoot) {
-	try {
-		for (i in storageFilePaths) {
-			await uploadFile(storageFilePaths[i],packFilePaths[i],packRoot)
+	return new Promise((resolve,reject) => {
+		try {
+			for (i in storageFilePaths) {
+				await uploadFile(storageFilePaths[i],packFilePaths[i],packRoot)
+			}
+			resolve("files uploaded")
+		} catch {
+			console.log(err)
+			reject("fail")
 		}
-	} catch {
-		console.log(err)
+	})
+}
+
+async function addFilesGetDownload (selectedModules) {
+
+	try {
+
+	// generate id and create pack path
+	const id = Nanoid.nanoid(5)	
+	const packPath = `/packs/LittleImprovementsCustom_${id}`
+	console.log("pack path = "+packPath)
+
+	// add modules files to pack
+	for (i in availableModules) {
+		if (selectedModules.includes (availableModules[i].id)) {
+		  await uploadMultipleFiles(availableModules[i].storageFiles,availableModules[i].packFiles,packPath)
+		}
 	}
+
+	// add pack.mcmeta file
+	await uploadFile("storage/pack.mcmeta","/pack.mcmeta",packPath)
+
+	// get share link and return it
+	dbx.sharingCreateSharedLink({path: packPath})
+	return response.url.slice(0, -1)+"1"
+
+	} catch {console.log(err)}
+
 }
 
 // setup express
@@ -47,36 +78,38 @@ app.use(express.static("public"));
 app.post('/', function (req, res) {
   console.log(req.body)
   if (req.body.new=="true") {
-    // generate id and create pack path
-    const id = Nanoid.nanoid(5)	
-    const packPath = `/packs/LittleImprovementsCustom_${id}`
-    console.log("pack path = "+packPath)
+    // // generate id and create pack path
+    // const id = Nanoid.nanoid(5)	
+    // const packPath = `/packs/LittleImprovementsCustom_${id}`
+    // console.log("pack path = "+packPath)
 
-    // ADD PACK FILES
-    // go through every available module, and if it is included in the request body, run the function to add it
-    for (i in availableModules) {
-      if (req.body.modules.includes (availableModules[i].id)) {
-        uploadMultipleFiles(availableModules[i].storageFiles,availableModules[i].packFiles,packPath)
-      }
-    }
+    // // ADD PACK FILES
+    // // go through every available module, and if it is included in the request body, run the function to add it
+    // for (i in availableModules) {
+    //   if (req.body.modules.includes (availableModules[i].id)) {
+    //     uploadMultipleFiles(availableModules[i].storageFiles,availableModules[i].packFiles,packPath)
+    //   }
+    // }
 
-    // add pack.mcmeta file, and create sharing link
-    fs.readFile("storage/pack.mcmeta", function (err, contents){
-      dbx.filesUpload({ path: packPath+"/pack.mcmeta", contents: contents })
-      .then(function (response) {
-        console.log(response);
-        dbx.sharingCreateSharedLink({path: packPath})
-        .then(function(response) {
-          res.send(response.url.slice(0, -1)+"1")
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-    })
+    // // add pack.mcmeta file, and create sharing link
+    // fs.readFile("storage/pack.mcmeta", function (err, contents){
+    //   dbx.filesUpload({ path: packPath+"/pack.mcmeta", contents: contents })
+    //   .then(function (response) {
+    //     console.log(response);
+    //     dbx.sharingCreateSharedLink({path: packPath})
+    //     .then(function(response) {
+    //       res.send(response.url.slice(0, -1)+"1")
+    //     })
+    //     .catch(function(error) {
+    //       console.log(error);
+    //     });
+    //   })
+    //   .catch(function (err) {
+    //     console.log(err);
+    //   });
+	// })
+	
+	res.send(addFilesGetDownload(req.body.modules))
     
     } else {
       res.send('sorry ur bad');
