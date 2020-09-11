f2b0de0d5d1e316f4eddc09767caf31932ad1ea8
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-11 07:14:31 +0100

    clean up client-side js files

--- public/js/index.js
+++ public/js/index.js
@@ -1,61 +1,61 @@
 // SELECT OR UNSELECT A MODULE WHEN THE USER CLICKS THE SELECTOR
 let selectedModules = []
 function toggleSelected(id) {
-    const theDiv = document.getElementById(id)
-    if (theDiv.classList.contains("unselected")) {
-        // the box is currently unselected; select it
-        theDiv.classList.remove("unselected")
-        theDiv.classList.add("selected")
-        selectedModules.push(id)
-    } else {
-        // the box is currently selected; unselect it
-        theDiv.classList.remove("selected")
-        theDiv.classList.add("unselected")
-        selectedModules.pop(id)
-    }
+	const theDiv = document.getElementById(id)
+	if (theDiv.classList.contains("unselected")) {
+		// the box is currently unselected; select it
+		theDiv.classList.remove("unselected")
+		theDiv.classList.add("selected")
+		selectedModules.push(id)
+	} else {
+		// the box is currently selected; unselect it
+		theDiv.classList.remove("selected")
+		theDiv.classList.add("unselected")
+		selectedModules.pop(id)
+	}
 }
 
 // HIDE THE IMAGE AND SHOW THE DESCRIPTION WHEN THE USER HOVERS OVER A SELECTOR
 function mouseOver() {
-    document.getElementById(this.id+"Img").classList.add("invisible")
-    document.getElementById(this.id+"Desc").classList.remove("invisible")
+	document.getElementById(this.id+"Img").classList.add("invisible")
+	document.getElementById(this.id+"Desc").classList.remove("invisible")
 }
 function mouseOut() {
-    document.getElementById(this.id+"Img").classList.remove("invisible")
-    document.getElementById(this.id+"Desc").classList.add("invisible")
+	document.getElementById(this.id+"Img").classList.remove("invisible")
+	document.getElementById(this.id+"Desc").classList.add("invisible")
 }
 
 // HANDLE THE USER PRESSING THE DOWNLOAD BUTTON
 function downloadPack() {
-    // stop the user downloading a pack with nothing selected
-    if (selectedModules.length==0) {
-        // let the user know they can't download a pack with no modules
-        alert("You can't download a pack with nothing selected.")
-        // return, so the post request is not sent
-        return
-    }
+	// stop the user downloading a pack with nothing selected
+	if (selectedModules.length==0) {
+		// let the user know they can't download a pack with no modules
+		alert("You can't download a pack with nothing selected.")
+		// return, so the post request is not sent
+		return
+	}
 
-    // show the download toast
-    document.getElementById("download-toast").classList.remove("invisible")
+	// show the download toast
+	document.getElementById("download-toast").classList.remove("invisible")
 
-    // send post request to the server
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://www.littleimprovements-custom.tk/", false);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify( {"new":"true","modules":selectedModules} ));
+	// send post request to the server
+	var xhr = new XMLHttpRequest()
+	xhr.open("POST", "https://www.littleimprovements-custom.tk/", false)
+	xhr.setRequestHeader("Content-Type", "application/json")
+	xhr.send(JSON.stringify( {"new":"true","modules":selectedModules} ))
 
-    // show fail toast if the response was "error"
-    if (xhr.response == "error") {
-        // hide the download toast
-        document.getElementById("download-toast").classList.add("invisible")
-        // show fail toast
-        document.getElementById("fail-toast").classList.remove("invisible")
-        // return, so the user doesnt get redirected
-        return
-    }
+	// show fail toast if the response was "error"
+	if (xhr.response == "error") {
+		// hide the download toast
+		document.getElementById("download-toast").classList.add("invisible")
+		// show fail toast
+		document.getElementById("fail-toast").classList.remove("invisible")
+		// return, so the user doesnt get redirected
+		return
+	}
     
-    // download
-    window.location.href = xhr.response
+	// download
+	window.location.href = xhr.response
 }
 
 
@@ -64,54 +64,54 @@ function downloadPack() {
 // function to add html elements
 function createModuleSelector(moduleid, modulelabel, icontype, moduledesc) {
 
-    const div = document.createElement("div");
-    div.setAttribute("class","grid-item selection-box unselected");
-    div.setAttribute("onclick", `javascript: toggleSelected('${moduleid}')`)
-    div.setAttribute("id", moduleid)
-    document.getElementById("pack-selector-container").appendChild(div)
+	const div = document.createElement("div")
+	div.setAttribute("class","grid-item selection-box unselected")
+	div.setAttribute("onclick", `javascript: toggleSelected('${moduleid}')`)
+	div.setAttribute("id", moduleid)
+	document.getElementById("pack-selector-container").appendChild(div)
 
-    const label = document.createElement("p")
-    label.setAttribute("class", "pack-label")
-    label.appendChild(document.createTextNode(modulelabel))
-    div.appendChild(label)
+	const label = document.createElement("p")
+	label.setAttribute("class", "pack-label")
+	label.appendChild(document.createTextNode(modulelabel))
+	div.appendChild(label)
 
-    const icon = document.createElement("img")
-    icon.setAttribute("class","pack-icon")
-    icon.setAttribute("src",`icons/${moduleid}.${icontype}`)
-    icon.setAttribute("id",moduleid+"Img")
-    div.appendChild(icon)
+	const icon = document.createElement("img")
+	icon.setAttribute("class","pack-icon")
+	icon.setAttribute("src",`icons/${moduleid}.${icontype}`)
+	icon.setAttribute("id",moduleid+"Img")
+	div.appendChild(icon)
 
-    const desc = document.createElement("p")
-    desc.setAttribute("class","pack-desc invisible")
-    desc.setAttribute("id", moduleid+"Desc")
-    desc.appendChild(document.createTextNode(moduledesc))
-    div.appendChild(desc);
+	const desc = document.createElement("p")
+	desc.setAttribute("class","pack-desc invisible")
+	desc.setAttribute("id", moduleid+"Desc")
+	desc.appendChild(document.createTextNode(moduledesc))
+	div.appendChild(desc)
 
-    div.addEventListener("mouseover", mouseOver)
-    div.addEventListener("mouseout", mouseOut)
+	div.addEventListener("mouseover", mouseOver)
+	div.addEventListener("mouseout", mouseOut)
 
 }
 
 // function to read JSON file containing data
 function loadJSON(callback) {   
-    const xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', '/api/modules', true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
+	const xobj = new XMLHttpRequest()
+	xobj.overrideMimeType("application/json")
+	xobj.open("GET", "/api/modules", true)
+	xobj.onreadystatechange = function () {
+		if (xobj.readyState == 4 && xobj.status == "200") {
+			// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+			callback(xobj.responseText)
+		}
+	}
+	xobj.send(null)
 }
 
 // function to load JSON then send to be added to HTML
 loadJSON(function(response) {
-    // Parse JSON string into object
-    var actual_JSON = JSON.parse(response);
-    for (i in actual_JSON) {
-        const data = actual_JSON[i]
-        createModuleSelector(data.id,data.label,data.icontype,data.description)
-    }
+	// Parse JSON string into object
+	var actual_JSON = JSON.parse(response)
+	for (i in actual_JSON) {
+		const data = actual_JSON[i]
+		createModuleSelector(data.id,data.label,data.icontype,data.description)
+	}
 })
--- public/js/theme.js
+++ public/js/theme.js
@@ -1,7 +1,7 @@
-if (window.matchMedia('(prefers-color-scheme: dark)').media === 'not all') {
-    document.documentElement.style.display = 'none';
-    document.head.insertAdjacentHTML(
-        'beforeend',
-        '<link rel="stylesheet" href="css/light.css" onload="document.documentElement.style.display = \'\'">'
-    );
+if (window.matchMedia("(prefers-color-scheme: dark)").media === "not all") {
+	document.documentElement.style.display = "none"
+	document.head.insertAdjacentHTML(
+		"beforeend",
+		"<link rel=\"stylesheet\" href=\"css/light.css\" onload=\"document.documentElement.style.display = ''\">"
+	)
 }
