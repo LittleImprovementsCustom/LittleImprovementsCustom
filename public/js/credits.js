f3528b14abb39d0413d946c2e37ccd1163970838
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-10-06 22:16:14 +0100

    some changes to credits page
    - update credits.js to use same format as index.js
    - added groups "team" and "contributor" to credits.json which reflect as headers on credits page of website (default contributor)
    - added "type" to credits.json, either "dynamic" or "static"
    - added "avatar" to credits.json if using static avatar
    - static option reads from local file in /public/avatars/avatar.png

--- public/js/credits.js
+++ public/js/credits.js
@@ -1,48 +1,64 @@
 // function to add html elements
-function createModuleSelector(name, userlink, uuid, userdesc) {
+function createModuleSelector(data) {
 
 	const div = document.createElement("div")
-	div.setAttribute("class","grid-item selection-box selectable unselected box-height")
-	div.setAttribute("onclick", `location.href='${userlink}'`)
+	div.setAttribute("class","grid-item selection-box unselected box-height")
+	div.setAttribute("onclick", `location.href='${data.link}'`)
 	document.getElementById("pack-selector-container").appendChild(div)
 
 	const label = document.createElement("p")
 	label.setAttribute("class", "pack-label")
-	label.appendChild(document.createTextNode(name))
+	label.appendChild(document.createTextNode(data.name))
 	div.appendChild(label)
 
 	const icon = document.createElement("img")
 	icon.setAttribute("class","pack-icon")
-	icon.setAttribute("src",`https://crafatar.com/renders/head/${uuid}`)
+	if (data.type=="dynamic") icon.setAttribute("src",`https://crafatar.com/avatars/${data.uuid}`)
+	else icon.setAttribute("src",`avatars/${data.avatar}`)
 	div.appendChild(icon)
 
 	const desc = document.createElement("p")
 	desc.setAttribute("class","pack-desc")
-	desc.appendChild(document.createTextNode(userdesc))
+	desc.appendChild(document.createTextNode(data.desc))
 	div.appendChild(desc)
 
 }
 
-// function to read JSON file containing data
-function loadJSON(callback) {   
-	const xobj = new XMLHttpRequest()
-	xobj.overrideMimeType("application/json")
-	xobj.open("GET", "/api/credits", true)
-	xobj.onreadystatechange = function () {
-		if (xobj.readyState == 4 && xobj.status == "200") {
-			// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-			callback(xobj.responseText)
-		}
-	}
-	xobj.send(null)
+// function to group headers
+function createGroupHeader (group) {
+
+	const div = document.createElement("div")
+	div.setAttribute("class","grid-item selection-box section-header unselected")
+	document.getElementById("pack-selector-container").appendChild(div)
+
+	const label = document.createElement("p")
+	label.appendChild(document.createTextNode(group))
+	div.appendChild(label)
+
 }
 
-// function to load JSON then send to be added to HTML
-loadJSON(function(response) {
-	// Parse JSON string into object
-	var actualJSON = JSON.parse(response)
-	for (i in actualJSON) {
-		const data = actualJSON[i]
-		createModuleSelector(data.name,data.link,data.uuid,data.desc)
+// function to read JSON file containing data
+const xobj = new XMLHttpRequest()
+xobj.overrideMimeType("application/json")
+xobj.open("GET", "/api/credits", true)
+xobj.onreadystatechange = function () {
+	if (xobj.readyState == 4 && xobj.status == "200") {
+
+		//get JSON and add to HTML
+		creditsJSON = JSON.parse(xobj.responseText) // parse JSON string into object
+		let groups = {
+			"team": [],
+			"contributor": []
+		}
+		for (i of creditsJSON) {
+			if (i.group=="team") groups.team.push(i)
+			else if (i.group=="contributor") groups.contributor.push(i)
+			else groups.contributor.push(i)
+		}
+		createGroupHeader("The Team")
+		for (data of groups.team) {createModuleSelector(data)}
+		createGroupHeader("Other Contributors")
+		for (data of groups.contributor) {createModuleSelector(data)}
 	}
-})
+}
+xobj.send(null)
