aa5f510998dea2a1d1c14f753bb9698b65af6b03
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-11-07 09:09:00 +0000

    #76 add clearDropbox.js
    also changed around scripts in package.json

---
+++ clearDropbox.js
@@ -0,0 +1,12 @@
+// require modules
+const Dropbox = require("dropbox").Dropbox
+require("isomorphic-fetch")
+require("dotenv").config()
+
+// setup Dropbox
+const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN })
+
+// delete folder containing packs
+const packsPath = "/packs"
+dbx.filesDeleteV2({path:packsPath})
+	.then( () => console.log(`successfully deleted ${packsPath} on Dropbox`) )
