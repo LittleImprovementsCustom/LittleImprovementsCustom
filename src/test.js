42f22fe4444a611d7f33c63afb4534fc43f3f2a1
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-12-02 23:34:23 +0000

    fix file paths in testing

--- src/test.js
+++ src/test.js
@@ -9,7 +9,7 @@ const dbx = new Dropbox ({ fetch: fetch, accessToken: process.env.DBXACCESSTOKEN
 dbx.checkUser({query: "testing dropbox app"}).then(res=>console.log(res))
 
 // check storage/data
-JSON.parse(fs.readFileSync("storage/data/categories.json"))
-JSON.parse(fs.readFileSync("storage/data/credits.json"))
-JSON.parse(fs.readFileSync("storage/data/modules.json"))
-JSON.parse(fs.readFileSync("storage/data/requests.json"))
+JSON.parse(fs.readFileSync("../storage/data/categories.json"))
+JSON.parse(fs.readFileSync("../storage/data/credits.json"))
+JSON.parse(fs.readFileSync("../storage/data/modules.json"))
+JSON.parse(fs.readFileSync("../storage/data/requests.json"))
