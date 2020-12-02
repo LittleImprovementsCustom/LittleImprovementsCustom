5550f5a8a2b8b4dbb0b14e086a57d5e206e590ba
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-12-02 22:47:49 +0000

    move node server files to src

--- src/app.js
+++ src/app.js
@@ -219,9 +219,8 @@ app.post("/uploadpack", (req, res) => {
 
 
 // listen server with express
-const server = app.listen(process.env.PORT || 3000, () => console.log('Server running'))
+const server = app.listen(process.env.PORT || 3000, () => console.log("Server running"))
 
 // export express stuff for testing
 module.exports.app = app
 module.exports.close = () => server.close
-// server.close()
