c153d5089a114da62d222631a06730506d0e1b2b
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-12-14 00:07:34 +0000

    raise timeout to 5000ms for packCreation test
    fix #129

--- src/test/packCreation.js
+++ src/test/packCreation.js
@@ -15,7 +15,8 @@ for (i of modules) moduleIDs.push(i.id)
 moduleIDs = moduleIDs.sort(() => 0.5 - Math.random()) // shuffle array
 moduleIDs = moduleIDs.slice(0, 5) // get first 5 elements
 
-describe ("check webpage POST requests", ()=>{
+describe ("check webpage POST requests", function () {
+	this.timeout(5000)
 	it("POST /download", (done) => {
 		chai.request(index.app)
 			.post ("/download")
